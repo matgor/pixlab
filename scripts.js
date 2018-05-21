078f0b386a203722dccee85eb5bb01fd33b88042
matgor <mateusz.gorczyca@gmail.com>
2018-05-21 19:41:05 +0200

    added playlist and fixed responsiveness in navbar

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-//this is JS file
+function onMenuButtonClick() {
+    let x = document.getElementById("playlist_menu");
+    if (x.style.display === "none") {
+        x.style.display = "contents";
+    } else {
+        x.style.display = "none";
+    }
+}
